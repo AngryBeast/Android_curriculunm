2afb9931ba7b39909eeb2bc7fe143914cc4c2b60
AngryBeast <974635395@qq.com>
2020-07-24 22:03:36 +0800

    first commit

---
+++ Code04/app/src/main/java/com/example/angrybeast/code04/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.angrybeast.code04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String MESSAGE_STRING = "com.example.angrybeast.code04.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final EditText etMessage = findViewById(R.id.message);
+        Button btSend = findViewById(R.id.send_message);
+
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = etMessage.getText().toString();
+                //Toast.makeText(MainActivity.this,message,Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,MessageActivity.class);
+                intent.putExtra(MESSAGE_STRING,message);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Code06/app/src/main/java/com/example/angrybeast/code06/News.java
@@ -0,0 +1,40 @@
+package com.example.angrybeast.code06;
+
+public class News {
+    private String mTitle;
+    private String mAuthor;
+    private String mContent;
+    private int mImageId;
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public String getmAuthor() {
+        return mAuthor;
+    }
+
+    public void setmAuthor(String mAuthor) {
+        this.mAuthor = mAuthor;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
+
+    public void setmContent(String mContent) {
+        this.mContent = mContent;
+    }
+
+    public int getImageId() {
+        return mImageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.mImageId = imageId;
+    }
+}
