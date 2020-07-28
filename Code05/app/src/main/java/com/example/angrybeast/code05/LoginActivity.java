eceb69f611e71ae0735531969c13da039d238177
AngryBeast <974635395@qq.com>
2020-07-28 16:34:48 +0800

    Code 5 done

--- Code05/app/src/main/java/com/example/angrybeast/code05/LoginActivity.java
+++ Code05/app/src/main/java/com/example/angrybeast/code05/LoginActivity.java
@@ -2,13 +2,51 @@ package com.example.angrybeast.code05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Boolean bPwdSwitch = false;
+    private EditText etPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final ImageView ivPwdSwitch = findViewById(R.id.iv_pwd_switch);
+        etPwd = findViewById(R.id.et_pwd);
+
+        Button btn_log = findViewById(R.id.btn_log);
+
+        ivPwdSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bPwdSwitch = !bPwdSwitch;
+                if (bPwdSwitch){
+                    ivPwdSwitch.setImageResource(R.drawable.ic_visibility_black_24dp);
+                    etPwd.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }
+                else {
+                    ivPwdSwitch.setImageResource(R.drawable.ic_visibility_off_black_24dp);
+                    etPwd.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+                    etPwd.setTypeface(Typeface.DEFAULT);
+                }
+            }
+        });
+
+        btn_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this,"Login in!!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- Code05/app/src/main/java/com/example/angrybeast/code05/MainActivity.java
+++ Code05/app/src/main/java/com/example/angrybeast/code05/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.angrybeast.code05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        Toast.makeText(MainActivity.this,"Enter Login Acticity",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
 }
